205d0dd3f6e20b4209e602bc7c405d46afc08def
jaimealberto <jaime00>
2019-01-22 14:06:22 -0600

    cambios

--- htdocs/takepos/takepos.php
+++ htdocs/takepos/takepos.php
@@ -50,7 +50,7 @@ if (!empty($conf->global->MAIN_APPLICATION_TITLE)) $title = 'TakePOS - ' . $conf
 $head = '<meta name="apple-mobile-web-app-title" content="TakePOS"/>
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="mobile-web-app-capable" content="yes">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>';
+<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>';
 top_htmlhead($head, $title, $disablejs, $disablehead, $arrayofjs, $arrayofcss);
 
 ?>
